9e2d027ae669c2c68ed934e19708cf0df5561e6c
Divyansh Prakash <divyansh@redpanda.co.in>
2015-07-27 02:28:32 +0530

    initial commit

---
+++ src/yushing_twitter/core.clj
@@ -0,0 +1,27 @@
+(ns yushing-twitter.core
+  (:use [clojure.string :only (split join)]))
+
+(def big-text
+  (slurp "resources/hpl.txt"))
+
+(def sentences
+  (re-seq #"[A-Za-z \']+[\.]" big-text))
+
+(def sentence-db
+  (map (fn [sentence]
+         (rest (split (-> sentence
+                          .trim
+                          .toLowerCase)
+                      #"[\ .]+")))
+       sentences))
+
+(def short-sentence-db
+  (filter #(and (>= (count %) 3)
+                (<= (count %) 5))
+          sentence-db))
+
+(defn rand-poem []
+  (->> (shuffle short-sentence-db)
+       (take 4)
+       (map #(join " " %))
+       (join ", ")))
---
+++ test/yushing_twitter/core_test.clj
@@ -0,0 +1,7 @@
+(ns yushing-twitter.core-test
+  (:require [clojure.test :refer :all]
+            [yushing-twitter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
